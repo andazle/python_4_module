3140e07e5bde59d78059549e042f684cc2d8d65e
andazle1 <andryushka.kazakov.07@mail.ru>
2023-06-18 02:00:30 +0700

    initial commit

--- vk_bot/wiki.py
+++
@@ -1,8 +0,0 @@
-import wikipedia
-
-def get_wiki_article(article):
-    wikipedia.set_lang('ru')
-    try:
-        return f'{wikipedia.summary(article)} {wikipedia.page(article).links}'
-    except wikipedia.WikipediaException:
-        return 'Не удалось найти информацию о вашем запросе'
