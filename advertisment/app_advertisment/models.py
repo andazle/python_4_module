c475f40bfa80b607b0424ef44a82d8821b640ebf
andazle1 <andryushka.kazakov.07@mail.ru>
2023-08-03 18:25:40 +0700

    1

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField('название', max_length=128 )
+    des = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits = 10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметить, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})'
+
+    class Meta:
+        db_table ='advertisements'
